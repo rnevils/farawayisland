c32e24b7127b22e7a411b93326c5e41e5415ee7b
Ryan Nevils <ryan.nevils21@gmail.com>
2024-01-13 11:31:02 -0500

    remove comments

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,10 +1,1 @@
 // place files you want to import through the `$lib` alias in this folder.
-
-// the decision is do I use this directory (or lib/srever specivifcally) for my calculations? or just put it right in the results right?
-
-// would make sense to put here if utilities wer used accross app, but everything is really just in results, so like saying goes - i should put related code close together directory
-
-// yeah really don't need the server folder right? should be able to move stuff here
-
-// also is it any faster if its in this file vs on server side.?
-// probably faster local unless the device is weak so idk
